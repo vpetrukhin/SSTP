eacb7cb664a0ba0ac074e9e1db90549c3cc05730
Vasiliy Petrukhin <vpetruin@inbox.ru>
2024-03-24 00:14:52 +0300

    Настроил роутинг для всех страниц
    Также:
    - исправил подключение картинок;
    - добавил логотип

--- common/const.js
+++ common/const.js
@@ -1,6 +1,11 @@
 export const appConstants = {
   routes: {
     index: "/",
+    products: "/products",
+    manufacture: "/manufacture",
     services: "/services",
+    about: "/about",
+    certificates: "/certificates",
+    contacts: "/contacts",
   },
 };
--- scripts/Router.js
+++ scripts/Router.js
@@ -3,10 +3,20 @@ import Route from "route-parser";
 
 import MainPage from "../pages/main.template";
 import ServicesPage from "../pages/services.template";
+import ProductsPage from "../pages/products.template";
+import ManufacturePage from "../pages/manufacture.template";
+import AboutPage from "../pages/about.template";
+import CertificatesPage from "../pages/certificates.template";
+import ContactsPage from "../pages/contacts.template";
 
 export const routes = {
   Main: new Route(appConstants.routes.index),
   Services: new Route(appConstants.routes.services),
+  Products: new Route(appConstants.routes.products),
+  Manufacture: new Route(appConstants.routes.manufacture),
+  About: new Route(appConstants.routes.about),
+  Certificates: new Route(appConstants.routes.certificates),
+  Contacts: new Route(appConstants.routes.contacts),
 };
 
 export const render = (path) => {
@@ -17,6 +27,16 @@ export const render = (path) => {
     result = MainPage();
   } else if (routes.Services.match(path)) {
     result = ServicesPage();
+  } else if (routes.Products.match(path)) {
+    result = ProductsPage();
+  } else if (routes.Manufacture.match(path)) {
+    result = ManufacturePage();
+  } else if (routes.About.match(path)) {
+    result = AboutPage();
+  } else if (routes.Certificates.match(path)) {
+    result = CertificatesPage();
+  } else if (routes.Contacts.match(path)) {
+    result = ContactsPage();
   }
 
   document.querySelector("#content").innerHTML = result;
